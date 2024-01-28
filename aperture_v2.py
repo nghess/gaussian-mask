05d6dd894ab252565cd8a90dbfc38aea2c7f6f9e
Nate Gonzales-Hess <nhess@uoregon.edu>
2024-01-28 00:24:16 -0800

    aperture v1

--- aperture_v2.py
+++ aperture_v2.py
@@ -1,32 +1,44 @@
 import cv2
 import numpy as np
 
+"""
+Aperture Mask - Animated Demo
+"""
 
-size = 1024
+size = 256  # Window size
 origin = [size//2, size//2]  # Center of canvas
 corner = int(np.sqrt(((size//2)**2)*2))  # Distance between origin and corner
 
 
+loop = True
+counter = 0
+
+while loop:
+
+    if counter < corner:
+
+        canvas = np.zeros([size,size], dtype=np.uint8)
+        canvas = cv2.circle(canvas, origin, counter, 255, -1)
+        canvas = cv2.GaussianBlur(canvas, (25,25), 50)
+
+        cv2.imshow("aperture", canvas)
+        cv2.waitKey(1)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 
+    elif counter < corner*2:
 
-for ii in range(corner):
+        canvas = cv2.circle(canvas, origin, counter-corner, 0, -1)
+        canvas = cv2.GaussianBlur(canvas, (25,25), 50)
 
-    canvas = np.zeros([size,size], dtype=np.uint8)
-    canvas = cv2.circle(canvas, origin, ii, 255, -1)
-    canvas = cv2.GaussianBlur(canvas, (25,25), 50)
+        cv2.imshow("aperture", canvas)
+        cv2.waitKey(1)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-    cv2.imshow("aperture", canvas)
-    cv2.waitKey(1)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    elif counter >= corner*2:
+        counter = 0
 
-for ii in range(corner):
+    counter += 1
 
-    canvas = cv2.circle(canvas, origin, ii, 0, -1)
-    canvas = cv2.GaussianBlur(canvas, (25,25), 50)
-
-    cv2.imshow("aperture", canvas)
-    cv2.waitKey(1)
-    if cv2.waitKey(1) == ord('q'):
-        break

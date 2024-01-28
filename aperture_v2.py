50c40a52ee159a18e109fc23f857020304840a4a
Nate Gonzales-Hess <nhess@uoregon.edu>
2024-01-28 00:35:10 -0800

    aperture v2

--- aperture_v2.py
+++ aperture_v2.py
@@ -8,8 +8,9 @@ Aperture Mask - Animated Demo
 size = 1024  # Window size
 origin = [size//2, size//2]  # Center of canvas
 corner = int(np.sqrt(((size//2)**2)*2))  # Distance between origin and corner
+canvas = np.zeros([size,size], dtype=np.uint8)  # Array to draw on
 
-
+# Animation
 loop = True
 counter = 0
 
@@ -17,7 +18,6 @@ while loop:
 
     if counter < corner:
 
-        canvas = np.zeros([size,size], dtype=np.uint8)
         canvas = cv2.circle(canvas, origin, counter, 255, -1)
         canvas = cv2.GaussianBlur(canvas, (25,25), 50)
 
